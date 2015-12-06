c60f1b41d168c1d04507279adce73340adc0397a
Jim Arnell <jim@arnellconsulting.com>
2015-12-06 01:45:22 +0100

    no message

--- build.py
+++ build.py
@@ -9,13 +9,13 @@ import os, sys
 import urllib
 import shutil
 
-MACHINES_DIR = "machines.d"
+TARGET_DIR = "target"
 
 def clean( ):
 	# Clean first
-	if os.path.exists(MACHINES_DIR):
-		shutil.rmtree(MACHINES_DIR)
-	os.mkdir(MACHINES_DIR)
+	if os.path.exists(TARGET_DIR):
+		shutil.rmtree(TARGET_DIR)
+	os.mkdir(TARGET_DIR)
 
 def createDiscoveryToken( numberOfMachines ):
 	url = "https://discovery.etcd.io/new?size=" + str(numberOfMachines);
@@ -27,17 +27,21 @@ def createCloudConfig( configuration, template, discoveryToken, numberOfMachines
 	ip = configuration["ip"];
 	hostname = configuration["hostname"]
 	cloudConfig = Template(template).safe_substitute(
-		private_ipv4=ip, 
-		public_ipv4=ip,
-		hostname=hostname,
-		discoveryToken=discoveryToken,
-		numberOfMachines=numberOfMachines
+		PRIVATE_IPV4=ip, 
+		PUBLIC_IPV4=ip,
+		HOSTNAME=hostname,
+		DISCOVERY_TOKEN=discoveryToken,
+		NUMBER_OF_MACHINES=numberOfMachines
 	);
-	with open(MACHINES_DIR + '/' + hostname + '.yaml','w') as f:
+	hostDirectory = TARGET_DIR + '/' + hostname
+	os.mkdir(hostDirectory)
+	with open(hostDirectory + '/cloud-config.yaml','w') as f:
 		f.write(cloudConfig)
+	shutil.copy('templates/install.sh', hostDirectory + '/install.sh')
+
 
 def build():
-	with open("configuration.yaml", 'r') as machinesStream, open("cloud-config.yaml") as templateStream:
+	with open("configuration.yaml", 'r') as machinesStream, open("templates/cloud-config.yaml") as templateStream:
 		configurations = yaml.load(machinesStream)	
 		template = templateStream.read()
 
